85b3a3886e9d826da8984d90492bf8fb99ecf0c7
lacey0905 <lacey0905@naver.com>
2019-08-05 16:50:39 +0900

    18

--- react-app/src/components/toc.js
+++ react-app/src/components/toc.js
@@ -5,7 +5,22 @@ class TOC extends Component {
         var lists = [];
         var data = this.props.data;
         for(var i=0; i < data.length; i++){
-            lists.push(<li key={data[i].id}><a href={"/content"+data[i].id}>{data[i].title}</a></li>)
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        data-id={data[i].id}
+                        onClick={
+                            function(e){
+                                e.preventDefault();
+                                this.props.onChangePage(e.target.dataset.id);
+                            }.bind(this)
+                        }
+                    >
+                        {data[i].title}
+                    </a>
+                </li>
+            )
         }
         return (
             <nav>
